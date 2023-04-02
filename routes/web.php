1d652d8e21a2fd0b1d6d1f46b3480d353c1b97c5
Serkhio <pardinaserkhio@gmail.com>
2023-04-03 00:24:13 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LotController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LotController::class, 'index'])->name('lot.index');
+Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+
+Route::get('/lotcreate', [LotController::class, 'create'])->name('lot.create');
+Route::get('/categorycreate', [CategoryController::class, 'create'])->name('category.create');
+
+Route::post('/lot/store', [LotController::class, 'store'])
+    ->name('lot.store');
+Route::post('/category/store', [CategoryController::class, 'store'])
+    ->name('category.store');
+
+Route::delete('/lot/{lot}', [LotController::class, 'destroy'])
+    ->name('lot.destroy');
+Route::delete('/category/{category}', [CategoryController::class, 'destroy'])
+    ->name('category.destroy');
+
+Route::get('/lot/edit/{lot}', [LotController::class, 'edit'])
+    ->name('lot.edit');
+Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])
+    ->name('category.edit');
+
+
+Route::put('/lot/{lot}', [LotController::class, 'update'])
+    ->name('lot.update');
+Route::put('/category/{category}', [CategoryController::class, 'update'])
+    ->name('category.update');
